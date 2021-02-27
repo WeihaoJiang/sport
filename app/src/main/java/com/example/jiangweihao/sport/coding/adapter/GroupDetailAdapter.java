4e9b8537f7aa959e5e9895fcd78c420786817f34
niufeiting <niufeiting@yidian-inc.com>
2021-02-27 20:25:16 +0800

    update:修改小组分队界面

--- app/src/main/java/com/example/jiangweihao/sport/coding/adapter/GroupDetailAdapter.java
+++ app/src/main/java/com/example/jiangweihao/sport/coding/adapter/GroupDetailAdapter.java
@@ -13,16 +13,17 @@ import com.avos.avoscloud.AVObject;
 import com.avos.avoscloud.AVQuery;
 import com.avos.avoscloud.AVUser;
 import com.example.jiangweihao.sport.R;
+import com.example.jiangweihao.sport.coding.bean.Activityinfo1;
 import com.example.jiangweihao.sport.coding.view.UserDialog;
 
 import java.util.ArrayList;
 
 public class GroupDetailAdapter extends RecyclerView.Adapter<GroupDetailAdapter.Holder>  {
     private Context mContext;
-    private ArrayList<AVObject> list;
+    private ArrayList<Activityinfo1> list;
     private UserDialog mNormalDialog;
 
-    public GroupDetailAdapter(Context mContext, ArrayList<AVObject> mlist) {
+    public GroupDetailAdapter(Context mContext, ArrayList<Activityinfo1> mlist) {
         this.mContext = mContext;
         this.list = mlist;
         mNormalDialog = new UserDialog(this.mContext);
@@ -37,55 +38,8 @@ public class GroupDetailAdapter extends RecyclerView.Adapter<GroupDetailAdapter.
 
     @Override
     public void onBindViewHolder(@NonNull Holder holder, final int i) {
-        final AVObject avObject = list.get(i);
-        holder.list_item_title.setText(avObject.get("username").toString());
-        holder.list_item_title.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                if (mNormalDialog == null) {
-                    mNormalDialog = new UserDialog(mContext);
-                }
-                mNormalDialog.setTv_submit("确定");
-                mNormalDialog.setOnNatvieListener(new UserDialog.OnNatvieListener() {
-                    @Override
-                    public void onNative(String s) {
-
-//                        AVQuery<AVObject> avQueryre = new AVQuery<>("_User");
-//                        avQueryre.getInBackground(avObject.getObjectId(), new GetCallback<AVObject>() {
-//                            @Override
-//                            public void done(AVObject avObject, AVException e) {
-//                               avObject.put("email","198518072@qq.com");
-//                               avObject.saveInBackground();
-//                            }
-//                        });
-                        // 第一参数是 className,第二个参数是 objectId
-                        AVObject todo = AVObject.createWithoutData("_User", list.get(i).getObjectId());
-
-                        // 修改 content
-                        todo.put("email",String.valueOf((int)(2+Math.random()*(10-1+1))));
-                        // 保存到云端
-                        todo.saveInBackground();
-
-                        AVQuery<AVUser> userQuery = new AVQuery<>("_User");
-
-                        Log.e("userQuery",userQuery.toString());
-//                        AVQuery.doCloudQueryInBackground("update _User set email='198518072@qq.com' where objectId='"+list.get(i).getObjectId()+"'", new CloudQueryCallback<AVCloudQueryResult>() {
-//                            @Override
-//                            public void done(AVCloudQueryResult avCloudQueryResult, AVException e) {
-//                                // 如果 e 为空，说明保存成功
-//                                if (e == null){
-//                                    ToastUtils.INSTANCE.showToast(mContext,"设置成功");
-//                                }
-//                            }
-//                        });
-                        mNormalDialog.dismiss();
-                    }
-                });
-                mNormalDialog.show();
-            }
-        });
-
+        final Activityinfo1 avObject = list.get(i);
+        holder.list_item_title.setText(avObject.getName()+"       " + avObject.getTime() + " 举办 ");
     }
 
 
